f835b4fb5bec57ab3c9b82df52f029e4afc249c9
SpentCoast <rgpirie07@gmail.com>
2026-01-31 22:15:41 -0500

    Added glm

--- premake5.lua
+++ premake5.lua
@@ -15,6 +15,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Lithen/vendor/GLFW/include"
 IncludeDir["Glad"] = "Lithen/vendor/Glad/include"
 IncludeDir["ImGui"] = "Lithen/vendor/imgui"
+IncludeDir["glm"] = "Lithen/vendor/glm"
 
 include "Lithen/vendor/GLFW"
 include "Lithen/vendor/Glad"
@@ -35,7 +36,9 @@ project "Lithen"
 	files
 	{
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
 	}
 
 	includedirs
@@ -44,7 +47,8 @@ project "Lithen"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 	links 
@@ -106,7 +110,8 @@ project "Sandbox"
 	includedirs
 	{
 		"Lithen/vendor/spdlog/include",
-		"Lithen/src"
+		"Lithen/src",
+		"%{IncludeDir.glm}"
 	}
 
 	links
