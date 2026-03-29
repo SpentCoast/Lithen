ee6cb3db173f7a9dd714cbf9d3ffb6e1698abd8c
SpentCoast <rgpirie07@gmail.com>
2026-03-29 17:58:38 -0400

    Fixed imgui not being able to commit

---
+++ Lithen/Vendor/imgui/Build-ImGui.lua
@@ -0,0 +1,26 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+    cppdialect "C++20"
+    staticruntime "Off"
+
+	targetdir ("%{wks.location}/Binaries/" .. OutputDir .. "/%{prj.name}")
+	objdir ("%{wks.location}/Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
