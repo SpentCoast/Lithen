ee8e928cb5be1d72980a9883b3fb8d8b70e80322
SpentCoast <rgpirie07@gmail.com>
2026-03-29 18:19:25 -0400

    Small tweak to the build system

---
+++ LithenExternal.lua
@@ -0,0 +1,10 @@
+group "Dependencies"
+	include "Lithen/Vendor/GLFW/Build-GLFW.lua"
+	include "Lithen/Vendor/Glad/Build-Glad.lua"
+	include "Lithen/Vendor/GLM/Build-GLM.lua"
+	include "Lithen/Vendor/imgui/Build-ImGui.lua"
+group ""
+
+group "Core"
+	include "Lithen/Build-Lithen.lua"
+group ""
